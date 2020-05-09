e694108e0fc6b5b6aafc839fde53df3be2c91e4f
kenjilam92 <kenji.lam.92@gmail.com>
2020-05-09 19:02:19 -0400

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ function App() {
 
     <h2>This is Class Components Assignment</h2>
     <div className="d-flex justify-content-around flex-wrap" >
-      <ProfolioC fname="Doe" lname="Jane" age="45" hair="black"/>
-      <ProfolioC fname="Smith" lname="John" age="88" hair="Brown"/>
-      <ProfolioC fname="Fillmore" lname="Millard" age="50" hair="Brown"/>
-      <ProfolioC fname="Doe" lname="Jane" age="45" hair="Brown"/>
+      <ProfolioC fname="Doe" lname="Jane" age={45} hair="black"/>
+      <ProfolioC fname="Smith" lname="John" age={88} hair="Brown"/>
+      <ProfolioC fname="Fillmore" lname="Millard" age={50} hair="Brown"/>
+      <ProfolioC fname="Doe" lname="Jane" age={45} hair="Brown"/>
     </div>
     <h2> This is Functional Components </h2>
     <div className="d-flex justify-content-around flex-wrap">
--- src/components/ClassCom.jsx
+++ src/components/ClassCom.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react"
 
 class ProfolioC extends Component{
+    constructor (props){
+      super(props);
+      this.state={
+        currentAge: this.props.age
+      }
+    }
+
+    happyBrithday =() => this.setState({
+      currentAge: this.state.currentAge+1
+    })
+
     render(){
       return (
-        <div className="container bg-success border border-dark" style={{width: '250px'}} >
+        <div className="container bg-success border border-dark" style={{width: '300px'}} >
           <h2>{this.props.fname}, {this.props.lname}</h2>
-          <p>Age: {this.props.age}</p>
+          <p>Age: {this.state.currentAge}</p>
           <p>Hair Color: {this.props.hair}</p>
+      <button class="btn btn-danger" onClick={this.happyBrithday}>Happy Birthday {this.props.fname} {this.props.lname}</button>
         </div>
       );
     }
